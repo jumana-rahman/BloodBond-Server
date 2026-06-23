465adafbfde9bf95face8caffecc3932d0d6a2ab
jumana-rahman <jumanarahman242@outlook.com>
2026-06-23 13:18:41 +0600

    store users information

--- index.js
+++ index.js
@@ -29,6 +29,51 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("bloodbond_db");
+    const usersCollection = database.collection("users");
+
+    app.post('/api/users/sync', async (req, res) => {
+      const user = req.body;
+
+      if (!user?.id) {
+        return res.status(400).send({ message: "User ID is required" });
+      }
+
+      const existingUser = await usersCollection.findOne({
+        _id: user.id
+      });
+
+      if (!existingUser) {
+        await usersCollection.insertOne({
+          _id: user.id,   // 🔥 IMPORTANT CHANGE
+          name: user.name,
+          email: user.email,
+          avatar: user.avatar,
+          bloodGroup: user.bloodGroup,
+          district: user.district,
+          upazila: user.upazila,
+
+          role: "donor",
+          status: "active",
+
+          createdAt: new Date()
+        });
+      }
+
+      res.send({ success: true });
+    });
+
+    app.get('/api/users/:id', async (req, res) => {
+      const id = req.params.id;
+
+      const user = await usersCollection.findOne({
+        _id: id
+      });
+
+      res.send(user);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
